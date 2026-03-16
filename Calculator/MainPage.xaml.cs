53c8b4ee2f6e38e8e90ad72ce1f20c7f8c3cf395
Moehussein1 <zestyian@gmail.com>
2026-03-16 13:32:19 +0100

    Add project files.

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace Calc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+             
+        }
+
+        private void OnPercentClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            EntryResult.Text += button.Text;
+
+
+
+        }
+        private void Operatorbuttons(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            EntryResult.Text += button.Text;
+        }
+}
